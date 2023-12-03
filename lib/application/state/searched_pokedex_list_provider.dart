2f5c17121a0a376cf45f1677738852769968edf1
asriel-dev-apps <t.t.dev.apps@gmail.com>
2023-12-03 22:18:31 +0900

    ファジーサーチ機能追加

--- lib/application/state/searched_pokedex_list_provider.dart
+++ lib/application/state/searched_pokedex_list_provider.dart
@@ -5,23 +5,48 @@ import '../../ui/widgets/pokedex_list_tile.dart';
 final sortedPokedexListTilesProvider =
     StateProvider.family<List<PokedexListTile>, List<PokedexListTile>>(
         (ref, pokedexListTiles) {
-  final listTiles = [...pokedexListTiles];
+  // Original List<PokedexListTile> を deep copy
+  final List<PokedexListTile> listTiles = [...pokedexListTiles];
+
+  // ファジーサーチにヒットしたリストタイルを返す
+  final String fuzzySearchString = ref.watch(fuzzySearchStringProvider);
+  List<PokedexListTile> fuzzySearchedListTiles = [];
+  // 入力されていない場合は全て返す
+  if (fuzzySearchString.isEmpty) {
+    fuzzySearchedListTiles = listTiles;
+  }
+  // ファジーサーチ実行
+  else {
+    // 正規表現（大文字/小文字を区別しない）
+    final regNotSensitive = RegExp(fuzzySearchString, caseSensitive: false);
+    fuzzySearchedListTiles = listTiles
+        .where((e) => e.result.name.contains(regNotSensitive))
+        .toList();
+  }
+
+  // ファジーサーチされたリストタイルを、現在のソートタイプによってソートして返す
   final sortType = ref.watch(pokedexListSortTypeProvider);
   switch (sortType) {
     case SortType.idASC:
-      listTiles.sort((a, b) => a.id.compareTo(b.id));
-      return listTiles;
+      fuzzySearchedListTiles.sort((a, b) => a.id.compareTo(b.id));
+      return fuzzySearchedListTiles;
     case SortType.idDESC:
-      listTiles.sort((a, b) => -a.id.compareTo(b.id));
-      return listTiles;
+      fuzzySearchedListTiles.sort((a, b) => -a.id.compareTo(b.id));
+      return fuzzySearchedListTiles;
     case SortType.nameASC:
-      listTiles.sort((a, b) => a.result.name.compareTo(b.result.name));
-      return listTiles;
+      fuzzySearchedListTiles
+          .sort((a, b) => a.result.name.compareTo(b.result.name));
+      return fuzzySearchedListTiles;
     case SortType.nameDESC:
-      listTiles.sort((a, b) => -a.result.name.compareTo(b.result.name));
-      return listTiles;
+      fuzzySearchedListTiles
+          .sort((a, b) => -a.result.name.compareTo(b.result.name));
+      return fuzzySearchedListTiles;
   }
 });
+
+// 検索ボックスに入力された文字列でファジーサーチをするための正規表現を返すProvider
+// 何も入力されていない場合: null
+final fuzzySearchStringProvider = StateProvider<String>((ref) => '');
 
 // 現在のソートタイプを返すProvider
 final pokedexListSortTypeProvider =
